23adb59435ff2e07c25373a1f17698bba5347b33
VINÃO <viniciuspaimdacosta@yahoo.com.br>
2024-04-11 19:35:30 -0300

    Vinicius

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>Home Page</title>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </head>
   <body>
-    
+    <div class="container my-5">
+      <h2>Choose a function:</h2>
+      <div></div>
+      <a class="btn btn-primary" href="pages/create.php" role="button">Post</a>
+      <br>
+      <div></div>
+      <a class="btn btn-primary" href="pages/list.php" role="button">List</a>
+      <br>
+      <div class="mb-3">
+        <a class="btn btn-primary" href="pages/show.php" role="button">Show</a>
+      </div>
+    </div>
   </body>
 </html>
